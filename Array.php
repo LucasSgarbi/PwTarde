0fa7c0d9f2f00554ab0fb55dad26418a4498df00
LucasSgarbi <ls.aravechia@hotmail.com>
2022-08-29 14:22:23 -0300

    tenatndo faze o index com as aulas

---
+++ Array.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /* 
+    $carros = array();
+    $carros = ["BMW","Gol","Saveiro","Hilux"];
+    echo "<ul>";
+    for ($i = 0;$i < count($carros);$i++)
+    {
+        echo "<li> $carros[$i] </li>";
+    }
+    echo "</ul>";
+    echo "<pre>";
+    print_r($carros);
+    echo "</pre>";
+
+    $nomes = array("Fernando","Isabela","Sabrina","Renato");
+    echo "<pre>";
+    print_r($nomes);
+    echo "</pre>";
+
+    array_push($nomes,"Bruno");
+    echo "<pre>";
+    print_r($nomes);
+    echo "</pre>";
+
+    $nomes[140]= "Robson";
+    echo "<pre>";
+    print_r($nomes);
+    echo "</pre>";
+
+    array_push($nomes,"Jorge");
+    echo "<pre>";
+    print_r($nomes);
+    echo "</pre>";
+    
+    
+    $nomes[5]= "Julia";
+    echo "<pre>";
+    print_r($nomes);
+    echo "</pre>";
+
+    array_push($nomes,"Lucas");
+    echo "<pre>";
+    print_r($nomes);
+    echo "</pre>";
+    
+    $carros["Eletricos"] = "Tesla";
+
+    echo "<pre>";
+    print_r($carros);
+    echo "</pre>";
+
+    $cadastro = array();
+    $pessoa1 = array("Id" => 1 , "Nome" => "Fer" , "CPF" => "000.000.000-01");
+    $pessoa2 = array("Id" => 2 , "Nome" => "Lucas" , "CPF" => "000.000.000-02");
+    $pessoa3 = array("Id" => 3 , "Nome" => "Carlos" , "CPF" => "000.000.000-03");
+
+    $cadastro["Cliente"]=$pessoa1;
+    array_push($cadastro, $pessoa2);
+    array_push($cadastro, $pessoa3);
+
+    echo "<pre>";
+    print_r($cadastro);
+    echo "</pre>";
+*/
+$num = array();
+for($i=0 ;$i<10;$i++)
+{
+    $num[$i] =rand(-10,10);
+}
+    echo "<pre>";
+    print_r($num);
+    echo "</pre>";
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,48 +1,17 @@
-<!DOCTYPE html>
+<!DOCTYPE php>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="estilo.css".css">
     <link rel="Script" href="script.js" >
+    <link rel="stylesheet" href="estilo.css" >
 </head>
 <body>
     <?php
-    echo "<h1> Meu HTML </h1>";
-
-    $nome= "Lucas";
-    $idade = 20 ;
-    $altura = 1.77 ;
-    $encalhado = true;
-    
-    $p1=8;
-    $p2=7.5;
-    CONST Nomecompleto = "Lucas Sgarbi Aravéchia";
-    echo Nomecompleto;
+    echo "<a href='Array.php'> Aula de Arry </a>";
     echo "<br>";
-    $media = ($p1 + $p2)/2;
-    if($media >= 6)
-    {
-        echo "Aprovado";
-    }
-    else
-    {
-        echo "Reprovavel";
-    }
-    echo "<br>";
-// Concatenar é . 
-
-   for($i =1 ; $i< 5197 ; $i++)
-    {
-        if($i%2==0)
-        {
-            echo $i. "\n";
-        }
-    }
-
     ?>
-
 </body>
-
+</html>
